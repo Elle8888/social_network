5a03e8ae7802f9586191489a1d432e821f4d3f8d
Elle <lou.d.b.rigodon@gmail.com>
2022-11-24 22:24:39 +0000

    modified seed files

--- spec/seeds_posts.sql
+++ spec/seeds_posts.sql
@@ -1,7 +1,9 @@
-TRUNCATE TABLE posts RESTART IDENTITY; -- replace with your own table name.
+TRUNCATE TABLE user_accounts, posts RESTART IDENTITY; -- replace with your own table name.
 
 -- Below this line there should only be `INSERT` statements.
 -- Replace these statements with your own seed data.
 
+INSERT INTO user_accounts (email_add, username) VALUES ('abc@gmail.com', 'abc');
+INSERT INTO user_accounts (email_add, username) VALUES ('def@gmail.com', 'def');
 INSERT INTO posts (title, content, views, user_accounts_id) VALUES ('title1', 'content1', 1, 1);
-INSERT INTO posts (title, content, views, user_accounts_id) VALUES ('title2', 'content2', 2, 2);
+INSERT INTO posts (title, content, views, user_accounts_id) VALUES ('title2', 'content2', 2, 1);
