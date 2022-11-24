23a63775e7f42f52d660104fef0054657c3c368c
Elle <lou.d.b.rigodon@gmail.com>
2022-11-24 17:22:31 +0000

    modified user_accounts

--- lib/user_account_repository.rb
+++ lib/user_account_repository.rb
@@ -23,31 +23,39 @@ class UserAccountsRepository
       end
     #   p accounts
       return accounts
-        
+
       # Returns an array of UserAccount objects.
     end
   
     # Gets a single record by its ID
     # One argument: the id (number)
     def find(id)
-      # Executes the SQL query:
-      # SELECT id,  email_add, username FROM user_accounts WHERE id = $1;
-  
+        sql = 'SELECT id, email_add, username FROM user_accounts WHERE id = $1;'
+        sql_params = [id]
+
+        result_set = DatabaseConnection.exec_params(sql, sql_params)
+        return result_set
       # Returns a single UserAccount object.
     end
   
     def create(user_account)
-      # INSERT INTO UserAccount
-      # (email_add, username)
-      # VALUES (user_account.email_add, user_account.username)
-      # returns nil
+      sql = 'INSERT INTO user_accounts (email_add, username)
+      VALUES ($1, $2);'
+
+      sql_params = [user_account.email_add, user_account.username]
+      result_set = DatabaseConnection.exec_params(sql, sql_params)
+      
+      return nil
     end
   
   #   # def update(student)
   #   # end
   
     def delete(id)
-      # DELETE FROM user_account WHERE id = $1'
-      # returns nil
+      sql = 'DELETE FROM user_accounts WHERE id = $1'
+      sql_params = [id]
+
+    result_set = DatabaseConnection.exec_params(sql, sql_params)
+    return nil
     end
   end
--- spec/user_accounts_spec.rb
+++ spec/user_accounts_spec.rb
@@ -33,6 +33,63 @@ RSpec.describe UserAccountsRepository do
         expect(user_accounts[1]['username']).to eq 'def'
 
     end
+
+    it 'gets a single user account' do
+
+        repo = UserAccountsRepository.new
+
+        user_accounts = repo.find(1)
+
+        # expect(user_accounts.length).to eq 2
+
+        expect(user_accounts[0]['id']).to eq '1'
+        expect(user_accounts[0]['email_add']).to eq 'abc@gmail.com'
+        expect(user_accounts[0]['username']).to eq 'abc'
+
+        user_accounts = repo.find(2)
+
+        expect(user_accounts[0]['id']).to eq '2'
+        expect(user_accounts[0]['email_add']).to eq 'def@gmail.com'
+        expect(user_accounts[0]['username']).to eq 'def'
+
+    end
+
+    it 'adds an object' do
+        repo = UserAccountsRepository.new
+
+        newAccount = UserAccount.new
+        newAccount.email_add = 'ghi@gmail.com'
+        newAccount.username = 'ghi'
+        newAccount.id = '3'
+
+        repo.create(newAccount)
+
+        user_accounts = repo.all
+
+        expect(user_accounts.length).to eq 3
+
+        expect(user_accounts[2]['id']).to eq '3'
+        expect(user_accounts[2]['email_add']).to eq 'ghi@gmail.com'
+        expect(user_accounts[2]['username']).to eq 'ghi'
+
+        # expect(newAccounts).to include(
+        #  have_attributes(
+        #    id: newAccount.id,
+        #    email_add: newAccount.email_add,
+        #    username: newAccount.username,
+        #    )
+        #  )
+    end
+
+    it 'deletes all acounts' do
+
+    repo = UserAccountsRepository.new
+    repo.delete(1)
+    repo.delete(2)
+    user_accounts = repo.all
+    expect(user_accounts.length).to eq 0
+
+    end
 end
 
 # Get all user_accounts
