0d708d24f53e87b283a13a699b252a5ed8fc6b4c
Elle <lou.d.b.rigodon@gmail.com>
2022-11-24 22:27:36 +0000

    added PostsRepository

---
+++ lib/posts_repository.rb
@@ -0,0 +1,59 @@
+require 'post'
+
+class PostsRepository
+
+  def all
+    sql = 'SELECT id, title, content, views, user_accounts_id FROM posts;'
+    result_set = DatabaseConnection.exec_params(sql, [])
+
+    posts = []
+
+    result_set.each do |entry|
+      post = Post.new
+
+      post.id = entry['id']
+      post.title = entry['title']
+      post.content = entry['content']
+      post.views = entry['views']
+      post.user_accounts_id = entry['user_accounts_id']
+
+      posts << post
+    end
+
+    return posts
+
+  end
+
+  def find(id)
+      sql = 'SELECT id, title, content, views, user_accounts_id FROM posts WHERE id = $1;'
+      sql_params = [id]
+      result_set = DatabaseConnection.exec_params(sql, sql_params)
+
+      post = Post.new
+
+      post.id = result_set[0]['id']
+      post.title = result_set[0]['title']
+      post.content = result_set[0]['content']
+      post.views = result_set[0]['views']
+      post.user_accounts_id = result_set[0]['user_accounts_id']
+
+      return post
+  end
+
+  def create(post)
+      sql = 'INSERT INTO posts (title, content, views, user_accounts_id)
+      VALUES ($1, $2, $3, $4);'
+      sql_params = [post.title, post.content, post.views, post.user_accounts_id]
+      result_set = DatabaseConnection.exec_params(sql, sql_params)
+      
+      return nil
+  end
+  
+  def delete(id)
+      sql = 'DELETE FROM posts WHERE id = $1'
+      sql_params = [id]
+      result_set = DatabaseConnection.exec_params(sql, sql_params)
+      
+      return nil
+  end
+end
---
+++ spec/posts_spec.rb
@@ -0,0 +1,107 @@
+require 'posts_repository'
+
+RSpec.describe PostsRepository do
+
+  def reset_posts_table
+    seed_sql = File.read('spec/seeds_posts.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'social_network_test' })
+    connection.exec(seed_sql)
+  end
+
+  before(:each) do
+    reset_posts_table
+  end
+
+  it 'gets all posts' do
+
+    repo = PostsRepository.new
+
+    post = repo.all
+
+    expect(post.length).to eq 2
+
+    expect(post[0].id).to eq '1'
+    expect(post[0].title).to eq 'title1'
+    expect(post[0].content).to eq 'content1'
+    expect(post[0].views).to eq '1'
+    expect(post[0].user_accounts_id).to eq '1'
+
+    expect(post[1].id).to eq '2'
+    expect(post[1].title).to eq 'title2'
+    expect(post[1].content).to eq 'content2'
+    expect(post[1].views).to eq '2'
+    expect(post[1].user_accounts_id).to eq '1'
+
+  end
+
+  it 'get a single user_account' do
+
+    repo = PostsRepository.new
+
+    post = repo.find(1)
+
+    expect(post.id).to eq '1'
+    expect(post.title).to eq 'title1'
+    expect(post.content).to eq 'content1'
+    expect(post.views).to eq '1'
+    expect(post.user_accounts_id).to eq '1'
+
+    post = repo.find(2)
+
+    expect(post.id).to eq '2'
+    expect(post.title).to eq 'title2'
+    expect(post.content).to eq 'content2'
+    expect(post.views).to eq '2'
+    expect(post.user_accounts_id).to eq '1'
+
+  end
+
+  it 'create a new object' do
+
+    repo = PostsRepository.new
+
+    post = Post.new
+
+    post.id = '3'
+    post.title =  'title3'
+    post.content =  'content3'
+    post.views = 3
+    post.user_accounts_id = 1
+
+    repo.create(post)
+
+    posts = repo.all
+
+    expect(posts.length).to eq 3
+
+    expect(posts[2].id).to eq '3'
+    expect(posts[2].title).to eq 'title3'
+    expect(posts[2].content).to eq 'content3'
+    expect(posts[2].views).to eq '3'
+    expect(posts[2].user_accounts_id).to eq '1'
+
+  end
+
+  it 'deletes all objects' do
+
+    repo = PostsRepository.new
+    repo.delete(1)
+    repo.delete(2)
+    post = repo.all
+    expect(post.length).to eq 0
+
+  end
+
+  it 'deletes one object' do
+
+    repo = PostsRepository.new
+    post = repo.all
+
+    expect(post.length).to eq 2
+    
+    repo.delete(1)
+    post = repo.all
+    expect(post.length).to eq 1
+
+  end
+end
