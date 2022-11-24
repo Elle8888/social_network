b723b956cb71a959723978aaaf562d5f7ad3f9e2
Elle <lou.d.b.rigodon@gmail.com>
2022-11-24 19:38:01 +0000

    adjusted spaces user_account_repository.rb

--- lib/user_account_repository.rb
+++ lib/user_account_repository.rb
@@ -2,60 +2,50 @@ require 'user_account'
 
 class UserAccountsRepository
 
-    # Selecting all records
-    # No arguments
-    def all
-      # Executes the SQL query:
-      sql = 'SELECT id, email_add, username FROM user_accounts;'
-      result_set = DatabaseConnection.exec_params(sql, [])
+  def all
+    sql = 'SELECT id, email_add, username FROM user_accounts;'
+    result_set = DatabaseConnection.exec_params(sql, [])
 
-      accounts = []
+    accounts = []
 
-      result_set.each do |account|
-        newAccount = UserAccount.new
+    result_set.each do |account|
+      newAccount = UserAccount.new
 
-        newAccount.id = account['id']
-        newAccount.email_add = account['email_add']
-        newAccount.username = account['username']
+      newAccount.id = account['id']
+      newAccount.email_add = account['email_add']
+      newAccount.username = account['username']
 
-        accounts << account
-        p account
-      end
-    #   p accounts
-      return accounts
+      accounts << account
 
-      # Returns an array of UserAccount objects.
     end
-  
-    # Gets a single record by its ID
-    # One argument: the id (number)
-    def find(id)
-        sql = 'SELECT id, email_add, username FROM user_accounts WHERE id = $1;'
-        sql_params = [id]
 
-        result_set = DatabaseConnection.exec_params(sql, sql_params)
-        return result_set
-      # Returns a single UserAccount object.
-    end
-  
-    def create(user_account)
-      sql = 'INSERT INTO user_accounts (email_add, username)
-      VALUES ($1, $2);'
+    return accounts
 
-      sql_params = [user_account.email_add, user_account.username]
-      result_set = DatabaseConnection.exec_params(sql, sql_params)
-      
-      return nil
-    end
-  
-  #   # def update(student)
-  #   # end
-  
-    def delete(id)
-      sql = 'DELETE FROM user_accounts WHERE id = $1'
-      sql_params = [id]
-
-    result_set = DatabaseConnection.exec_params(sql, sql_params)
-    return nil
-    end
   end
+
+  def find(id)
+    sql = 'SELECT id, email_add, username FROM user_accounts WHERE id = $1;'
+    sql_params = [id]
+    result_set = DatabaseConnection.exec_params(sql, sql_params)
+
+    return result_set
+
+  end
+
+  def create(user_account)
+    sql = 'INSERT INTO user_accounts (email_add, username)
+      VALUES ($1, $2);'
+    sql_params = [user_account.email_add, user_account.username]
+    result_set = DatabaseConnection.exec_params(sql, sql_params)
+
+    return nil
+  end
+
+  def delete(id)
+    sql = 'DELETE FROM user_accounts WHERE id = $1;'
+    sql_params = [id]
+    result_set = DatabaseConnection.exec_params(sql, sql_params)
+
+    return nil
+  end
+end
